7506a671e9a11217a9d0a25055bf1d8a03693016
realnotU <notyoudev@gmail.com>
2021-05-07 23:07:27 -0400

    IM GOING TO FUCKING KILL MYSELF

--- source/server.cpp
+++ source/server.cpp
@@ -5,49 +5,62 @@
 
 
 // error handling is for losers
+
 class Server {
     private:
-        int portaddr = 1337; // setting this up to be changeable programmatically        
-        int sockfd;
+        int portaddr = 3000; // setting this up to be changeable programmatically        
         int queuelim = 20; // set to 20 connections rn
+
+        // socket
         struct sockaddr_in serv_addr, cli_addr;
+        sockaddr_in sockaddr;
+        int sockfd;
+
+        void start_socket() {
+            sockaddr.sin_family = AF_INET;
+            sockaddr.sin_addr.s_addr = INADDR_ANY;
+            sockaddr.sin_port = htons(portaddr);
+        }
+
     public:
         int create_socket() {
+            this->start_socket();
             sockfd = socket (AF_INET, SOCK_STREAM, 0);
             if (sockfd == -1) {
                 std::cout << "Error while creating socket: " << errno << std::endl;
                 // exit handling here
-            } else {
-                return sockfd;
+                std::cout << EXIT_FAILURE << std::endl;
+                exit(EXIT_FAILURE);
             }
         }
 
         void start_listen() {
-            sockaddr_in sockaddr;
-            sockaddr.sin_family = AF_INET;
-            sockaddr.sin_addr.s_addr = INADDR_ANY;
-            sockaddr.sin_port = htons(this -> portaddr);
-
             // testing port
-            if (bind(this -> sockfd, (struct sockaddr*)&sockaddr, sizeof(sockaddr)) < 0) {
+            if (bind(sockfd, (struct sockaddr *) &sockaddr, sizeof(sockaddr)) < 0) {
                 std::cout << "Error while binding to port: " << errno << std::endl;
                 // exit handling here
+                exit(EXIT_FAILURE);
             }
 
             // hold queue
-            if (listen(this -> sockfd, this -> queuelim) < 0) {
+            if (listen(sockfd, queuelim) < 0) {
                 std::cout << "Error on listen: " << errno << std::endl;
                 // exit handling here
+                exit(EXIT_FAILURE);
             }
         }
 
         void handle() {
             // testing with one connection rn, will add actual handling later
             auto addrlen = sizeof(sockaddr);
-            int connection = accept(this -> sockfd, (struct sockaddr *) &sockaddr, &addrlen);
+
+            // fixing accept
+            int connection = accept(sockfd, (struct sockaddr *) &sockaddr, (socklen_t*)&addrlen);
+
             if (connection < 0) {
                 std::cout << "Error while grabbing connection: " << errno << std::endl;
                 // exit handling here
+                exit(EXIT_FAILURE);
             }
 
             // read connection
@@ -64,6 +77,6 @@ class Server {
         }
 
         void shutdown() {
-            close(this -> sockfd);
+            close(sockfd);
         }
 };
