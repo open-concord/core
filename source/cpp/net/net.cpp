66afe574f2408b08c4c65c61f58f9ac5abb508bd
LStack <laughablestack@protonmail.com>
2021-06-28 17:30:20 -0400

    fix net.cpp

--- source/cpp/net/net.cpp
+++ source/cpp/net/net.cpp
@@ -3,6 +3,7 @@
 #include <iostream> // For cout
 #include <unistd.h> // For read
 #include <string>
+#include "net.h"
 
 /**
  * Initiation of a Server instance
@@ -11,106 +12,95 @@
  * 
  * In either case, it's nessecary to provide a port number
  */
-class Server {
+
+Server::Server(int portaddr, int queuelim, bool local) {
+    this->portaddr = portaddr;
+    this->queuelim = queuelim;
+    if (local) {family = AF_LOCAL;}
+    this->initiate_socket();
+};
+
+void Server::initiate_socket() {
+    struct sockaddr_in serv_addr, cli_addr;
+
+    addr.sin_family = family;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(portaddr);
+
+    this->sockfd = socket (family, SOCK_STREAM, 0);
+
+    if (this->sockfd < 0) {
+        std::cout << "Error while creating socket: " << errno << std::endl;
+        // exit handling here
+        std::cout << EXIT_FAILURE << std::endl;
+        exit(EXIT_FAILURE);
+    }
+};
+
+void Server::open_listening() {
+            // binding port
+    if (bind(sockfd, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+        std::cout << "Error while binding to port: " << errno << std::endl;
+        // exit handling here
+        exit(EXIT_FAILURE);
+    }
+            
+    // checking socket listen
+    if (listen(sockfd, queuelim) < 0) {
+        std::cout << "Error on listen: " << errno << std::endl;
+        // exit handling here
+        exit(EXIT_FAILURE);
+    }
+};
+
+// Client Connection
+class Server::Client {
     private:
-        int portaddr; // port address of server instance        
-        int queuelim; // prospective limits on connection queue
-        int family = AF_INET; // default connection family is INET
-        struct sockaddr_in addr; // socket obj
-        int sockfd; // socket
+        // pointer to parent hub
+        Server * hub;
+        // setting 'important' vars
+        char buffer[256];
+        int connection;
+    public:
+        // each client object represents a connection to the hub from a user
+        // each client is created from the parent hub's connection queue
+        // basically hoisting the parent hub each time a new client is made
+        Client (Server * h) : hub(h) {
 
-        // socket initation
-        void intiate_socket() {
-            struct sockaddr_in serv_addr, cli_addr;
+            // pickup the next connection in queue
+            auto addrlen = sizeof(hub->addr);
 
-            addr.sin_family = family;
-            addr.sin_addr.s_addr = INADDR_ANY;
-            addr.sin_port = htons(portaddr);
+            // accept new connection
+            this->connection = accept(hub->sockfd, (struct sockaddr *) &hub->addr, (socklen_t*)&addrlen);
 
-            this->sockfd = socket (family, SOCK_STREAM, 0);
-
-            if (this->sockfd < 0) {
-                std::cout << "Error while creating socket: " << errno << std::endl;
+            if (this->connection < 0) {
+                std::cout << "Error while grabbing connection: " << errno << std::endl;
                 // exit handling here
-                std::cout << EXIT_FAILURE << std::endl;
                 exit(EXIT_FAILURE);
             }
+
+            // read connection
+            auto bytesread = read(connection, buffer, 255);
         }
 
-    public:
-        Server(int portaddr, int queuelim, bool local) {
-            this->portaddr = portaddr;
-            this->queuelim = queuelim;
-            if (local) {family = AF_LOCAL;}
-            this->intiate_socket();
-        };
+        // overwrite this function to take over input handling
+        void handle() {
+            std::cout << "Received: " << buffer;
+        }
 
-        void open_listening() {
-            // binding port
-            if (bind(sockfd, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
-                std::cout << "Error while binding to port: " << errno << std::endl;
-                // exit handling here
-                exit(EXIT_FAILURE);
-            }
-            
-            // checking socket listen
-            if (listen(sockfd, queuelim) < 0) {
-                std::cout << "Error on listen: " << errno << std::endl;
-                // exit handling here
-                exit(EXIT_FAILURE);
-            }
-        };
+        void close_connection(std::string receipt) {
+            // end communication w/ receipt
+            send(connection, receipt.c_str(), receipt.size(), 0);
+            // close socket connection
+            close(connection);
+        }
+};
 
-        // Client Connection
-        class Client {
-            private:
-                // pointer to parent hub
-                Server * hub;
-                // setting 'important' vars
-                char buffer[256];
-                int connection;
-            public:
-                // each client object represents a connection to the hub from a user
-                // each client is created from the parent hub's connection queue
-                // basically hoisting the parent hub each time a new client is made
-                Client (Server * h) : hub(h) {
-
-                    // pickup the next connection in queue
-                    auto addrlen = sizeof(hub->addr);
-
-                    // accept new connection
-                    this->connection = accept(hub->sockfd, (struct sockaddr *) &hub->addr, (socklen_t*)&addrlen);
-
-                    if (this->connection < 0) {
-                        std::cout << "Error while grabbing connection: " << errno << std::endl;
-                        // exit handling here
-                        exit(EXIT_FAILURE);
-                    }
-
-                    // read connection
-                    auto bytesread = read(connection, buffer, 255);
-                }
-
-                // overwrite this function to take over input handling
-                void handle() {
-                    std::cout << "Received: " << buffer;
-                }
-
-                void close_connection(std::string receipt) {
-                    // end communication w/ receipt
-                    send(connection, receipt.c_str(), receipt.size(), 0);
-                    // close socket connection
-                    close(connection);
-                }
-
-
-        };
-        void handle_next() {
-            // handle new block
-            Client new_connection(this);
-            new_connection.handle();
-        };
-        void shutdown() {
-            close(sockfd);
-        };
+void Server::handle_next() {
+    // handle new block
+    Client new_connection(this);
+    new_connection.handle();
+};
+void Server::shutdown() {
+    close(sockfd);
 };
