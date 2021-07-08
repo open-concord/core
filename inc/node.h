61b464f0df24f104ebd50f48bc42faf1cd7f15cb
realnotU <notyoudev@gmail.com>
2021-07-08 16:39:38 -0400

    node networking

--- inc/node.h
+++ inc/node.h
@@ -4,26 +4,69 @@
 #include <boost/beast/core.hpp>
 #include <boost/asio.hpp>
 #include <boost/asio/ssl.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/beast/ssl/ssl_stream.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
 #include <iostream>
 #include <thread>
+#include <memory>
+
+class Conn : public boost::enable_shared_from_this<Conn> {
+    private:
+        Conn(boost::asio::io_context& io_ctx);
+        boost::asio::ip::tcp::socket tsock;
+        std::string incoming_msg;
+    public:
+        // basically just a shared_ptr of self
+        typedef std::shared_ptr<Conn> ptr;
+        static ptr create(boost::asio::io_context& io_ctx);
+        boost::asio::ip::tcp::socket& socket();
+        void handle();
+        void error_handle(const boost::system::error_code& err);
+};
 
 class Node {
     private:
-        boost::asio::io_service local_io_service;
-        boost::asio::ip::tcp::acceptor local_acceptor;
+        // 'io_service' is deprecated
+
+        // context objs
+        boost::asio::io_context io_ctx;
+        boost::asio::ssl::context ssl_ctx{boost::asio::ssl::context::tlsv12};
+
+        // *not threadsafe* (like most networking i/o)
+        // docs mention using boost::asio::make_strand(ioc), maybe look into that later
+        boost::asio::ip::tcp::resolver res{io_ctx};
+
+        // endpoint + max queue
+        boost::asio::ip::tcp::endpoint endpoint;
+
+        boost::asio::ip::tcp::acceptor acceptor{io_ctx, endpoint};
+        unsigned short int queue;
+
+        // past connection retention
         std::string target_dir;
         struct khost {
             std::string address;
-            int port;
+            unsigned short int port;
             std::chrono::high_resolution_clock::time_point last_verfed;
-        }
+        };
         std::vector<khost> known_hosts;
-        void start_accept();
-        void handle_accept(boost:asio::ip::tcp::socket& handle_socket);
+
     public:
-        Node();
+        Node(unsigned short int queue, unsigned short int port);
 
-        void start(int accepting_port);
+        // start listening
+        void start();
 
+        // stop listening
         void stop();
+
+        // passive communication (eg traditional server role)
+        void begin_next();
+        
+        void handle_accept(Conn::ptr new_conn, const boost::system::error_code& err);
+        
+        // active communication (eg traditional client role)
+        void contact(std::string content, std::string ip, int port);
 };
--- src/node/node.cpp
+++ src/node/node.cpp
@@ -1,33 +1,97 @@
-#include <string>
-#include <vector>
-#include <chrono>
+// boost
 #include <boost/beast/core.hpp>
 #include <boost/asio.hpp>
 #include <boost/asio/ssl.hpp>
+#include <boost/bind.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/hof/placeholders.hpp>
+
+// std
+#include <string>
+#include <vector>
+#include <chrono>
 #include <iostream>
 #include <thread>
+#include <functional>
+#include "../../inc/node.h"
 
-Node::Node() {
-    (this->local_io_service).run();
+// Connection instance
+
+Conn::Conn(boost::asio::io_context& io_ctx) : tsock(io_ctx) {}
+
+Conn::ptr Conn::create(boost::asio::io_context& io_ctx) {
+    return ptr(new Conn(io_ctx));
+}
+boost::asio::ip::tcp::socket& Conn::socket() {
+    return this->tsock;
+}
+void Conn::handle() {
+    // https://www.boost.org/doc/libs/develop/libs/beast/doc/html/beast/ref/boost__beast__tcp_stream.html
+
+    // support for async read
+
+    // HANDLE HERE
+
+    // NOTE
+    // make sure to add the socket's info to this->khosts
+}
+// this is really jank, we should have seperate error handlers for each function
+void Conn::error_handle(const boost::system::error_code& err) {
+    // error handling here
 }
 
-void Node::start(int accepting_port) {
-    this->local_acceptor = boost::asio::ip::tcp::acceptor(this->local_io_service, tcp::endpoint(tcp::v4(), accepting_port));   
-
-    start_accept();
+// overarching node instance
+Node::Node(unsigned short int queue, unsigned short int port) {
+    boost::asio::ip::tcp::endpoint endpoint{boost::asio::ip::tcp::v4(), port};
+    this->queue = queue;
 }
 
+void Node::start() {
+    // acceptor setup
+    this->acceptor.open(this->endpoint.protocol());
+    this->acceptor.set_option(boost::asio::ip::tcp::acceptor::reuse_address(true));
+    this->acceptor.bind(this->endpoint);
+    this->acceptor.listen(this->queue);
+};
+
+// stop listen
 void Node::stop() {
-    (this->local_acceptor).cancel();
-}
+    // should be able to add graceful acceptor shutdown with boost::asio::bind_executor
+    // also make sure to close out any still-uncompleted async tasks
+    this->acceptor.cancel();
+    this->acceptor.close();
+};
+// awaits new connection and passes message to handler
+void Node::begin_next() {
+    // create new connection instance
+    Conn::ptr new_conn = Conn::create(this->io_ctx);
 
-void Node::start_accept() {
-    boost::asio::ip::tcp::socket handle_socket(this->local_io_service);
-    (this->local_acceptor).async_accept(handle_socket, boost::bind(&Node::handle, this, handle_socket));
-}
+    // await connection creation
+    this->acceptor.async_accept(new_conn->socket(),
+        boost::bind(&Node::handle_accept, this, new_conn,
+        boost::asio::placeholders::error)
+    );
+};
+// util func for async-call timing
+void Node::handle_accept(Conn::ptr new_conn, const boost::system::error_code& err) {
+    if (!err) {
+        new_conn->handle();
+        begin_next();
+    } else {
+        // kinda jank, we should really write seperate error handlers for each function
+        new_conn->error_handle(err);
+    }
+};
 
-void Node::handle(boost::asio::ip::tcp::socket& sock) {
-    start_accept(); //make sure new requests also get handled.
+// intiates contact with another ws
+void Node::contact(std::string content, std::string ip, int port) {
+    // target info
+    boost::asio::ip::tcp::endpoint ep(boost::asio::ip::address::from_string(ip), port);
+    // socket creation
+    boost::beast::ssl_stream<boost::beast::tcp_stream> sock {io_ctx, ssl_ctx};
 
-    //handling time
-}
+    sock.next_layer().connect(ep);
+
+    // can use sock.next_layer to send stuff (may be sync, idk about async outgoing)
+};
