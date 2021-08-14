653a418f337d5ce9e835352b0f91f2fc21464a58
LaughableStack <laughablestack@protonmail.com>
2021-08-14 16:36:08 -0400

    header changes

--- inc/tree.h
+++ inc/tree.h
@@ -1,8 +1,21 @@
 #include <vector>
 #include <array>
-#include "miner.h"
+#include <boost/function.hpp>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 #pragma once
+
+bool no_filter(json data);
+
+bool type_filter(char qtype, json data);
+
+json get_continuity_value(std::vector<json> defs,  std::string key);
+
+std::string chain_encrypt(json data, std::string dsa_pri_key, std::string rsa_pub_key, std::string aes_key, char mt);
+
+bool verify_block(std::vector<std::string> block, int pow);
 
 class Tree {
     private:
@@ -27,36 +40,11 @@ class Tree {
 
         std::vector<std::vector<std::string>> get_chain();
 
+        std::vector<json> search(char message_type, std::string target_trip, std::string key, boost::function<bool(json)> filter = no_filter, int start_b = -1, int end_b = -1);
+
         bool verify_chain();
 
         void chain_push(std::vector<std::string> block);
 
         void save_latest();
 };
-
-class FileTree {
-    private:
-    public:
-        Tree target_tree;
-        std::string target_dir;
-
-        FileTree();
-        FileTree(std::string dir);
-        void load(std::string dir);
-
-        void set_pow_req(int POW_req);
-
-        //Pass through normal classes.
-
-        void generate_branch(bool debug_info, std::string c1, std::string st, std::string creator_trip = std::string(24, '='));
-
-        std::vector<std::vector<std::string>> get_chain();
-
-        bool verify_chain();
-
-        void chain_push(std::vector<std::string> block);
-        
-        void save_latest();
-};
-
-bool verify_block(std::vector<std::string> block, int pow);
