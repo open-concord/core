c745a8c6fb3e21febaa922905429ce4085058f32
apius <notyoudev@gmail.com>
2022-07-18 12:33:16 -0400

    b/w bump

--- test/bw/src/watson.cpp
+++ test/bw/src/watson.cpp
@@ -12,23 +12,28 @@
 
 int main() {
     std::cout << " == initialising graph ==\n";
-    std::map<std::string, Tree> local_forest;
+    std::map<std::string, Tree*> local_forest;
     std::string tree_trip = gen::trip("seed");
-    local_forest[tree_trip] = Tree("./output/achain");
-    Tree& local_tree = local_forest[tree_trip];
+    
+    local_forest[tree_trip] = new Tree("./output/achain");
+    Tree& local_tree = *(local_forest[tree_trip]);
+    
     user local_user;
     local_tree.set_pow_req(3);
+    
     std::string aes_key = "Xv/azljSEXepU9ThHnfS6mKxLmiw0b90fMm6EsfXF5s=";
     Server local_server(local_tree, aes_key, local_user);
+    
     std::ifstream art_ifs("ascii.txt");
     std::stringstream art_buffer;
     art_buffer << art_ifs.rdbuf();
     json test_json;
     test_json["c"] = art_buffer.str(); 
     local_server.send_message(local_user, test_json, 'c');
+    
     std::cout << "== initialising node ==\n";  
     Node local_node(1338);
-    local_node.Graph.Forest = &local_forest;
+    local_node.Graph.Forest = local_forest;
     
     std::cout << "== contacting ==\n";
     local_node.Contact("127.0.0.1", 1337);
