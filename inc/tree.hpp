4162d5abf816f2267a4141e243186a1953089d96
LaughableStack <LaughableStack@protonmail.com>
2022-08-19 23:59:49 -0400

    update tree inc

--- inc/tree.hpp
+++ inc/tree.hpp
@@ -78,10 +78,10 @@ namespace std {
         }
     };
 }
-inline bool operator== (block a, block b) {
-  return (a.hash == b.hash);
-}
 
+bool operator== (block a, block b) {
+    return (a.hash == b.hash);
+}
 
 std::vector<std::string> order_hashes(std::unordered_set<std::string> input_hashes);
 
